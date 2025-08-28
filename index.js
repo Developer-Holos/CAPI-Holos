4e10241ca3770b9396a6bdbf6d8b8ac72320c465
Diego Larios <d.larios@rcs.com.pe>
2025-08-28 18:32:04 -0500

    Configuration first webhook

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 // const pool = require("./db");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // 🔧 Configuración
 const PIXEL_ID = '';
-const ACCESS_TOKEN = '';
-const VERIFY_TOKEN = "";
+const ACCESS_TOKEN = 'EAAO5UqGn1ZB0BPVserklJoUS8ZBmusJBxGZAZCw5MCZCT6M7z51mQNKa6QKauzVBCBFWg2qBPTBx0S2A4dm1kqHjmZBBkInQyZBrCGAahVLE2cw9G8RShaJ3MjZCC40kXZAtfxZAZBOZA0MhyX3L0becfHm6J0Q4bmxqHKyirnDyxQjvxWyHk5cCX8EofxX4XBo1Q1qDrwZDZD';
+const VERIFY_TOKEN = "capiHolosToken";
 
 // ✅ Verificación del Webhook
-app.get("/facebook/webhook", (req, res) => {
+app.get("/wstp/Holos", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
