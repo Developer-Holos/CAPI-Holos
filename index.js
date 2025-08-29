9bc1a40caba9303010eaf8cd4ce544a82ccf8fbd
Diego Larios <d.larios@rcs.com.pe>
2025-08-29 08:28:07 -0500

    Fix server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-// const pool = require("./db");
+const pool = require("./db");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
